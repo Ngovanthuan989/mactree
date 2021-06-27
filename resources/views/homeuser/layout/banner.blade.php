ed17408d0bdfd35ddcf39c64cacf3b352a625a8f
ngovanthuan <ngovanthuan989@gmail.com>
2021-06-27 19:01:32 +0700

    ke

--- resources/views/homeuser/layout/banner.blade.php
+++ resources/views/homeuser/layout/banner.blade.php
@@ -1,7 +1,7 @@
 <ul id="banners_grid" class="clearfix">
     <li>
         <a href="#0" class="img_container">
-            <img src="img/banners_cat_placeholder.jpg" data-src="{{asset('/PageUser/img/Banner2/1.png')}}" alt="" class="lazy">
+            <img src="img/banners_cat_placeholder.jpg" data-src="{{asset('/pageuser/img/Banner2/1.png')}}" alt="" class="lazy">
             <div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
                 <h3>Macbook air</h3>
                 <div><span class="btn_1">Mua ngay</span></div>
@@ -10,7 +10,7 @@
     </li>
     <li>
         <a href="#0" class="img_container">
-            <img src="img/banners_cat_placeholder.jpg" data-src="{{asset('/PageUser/img/Banner2/2.png')}}" alt="" class="lazy">
+            <img src="img/banners_cat_placeholder.jpg" data-src="{{asset('/pageuser/img/Banner2/2.png')}}" alt="" class="lazy">
             <div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
                 <h3>Macbook pro </h3>
                 <div><span class="btn_1">Mua ngay</span></div>
@@ -19,7 +19,7 @@
     </li>
     <li>
         <a href="#0" class="img_container">
-            <img src="img/banners_cat_placeholder.jpg" data-src="{{asset('/PageUser/img/Banner2/3.png')}}" alt="" class="lazy">
+            <img src="img/banners_cat_placeholder.jpg" data-src="{{asset('/pageuser/img/Banner2/3.png')}}" alt="" class="lazy">
             <div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
                 <h3>iPhone</h3>
                 <div><span class="btn_1">Mua ngay</span></div>
