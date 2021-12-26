9586628a73811bab7ffb4d27d0b9609de89b839c
ngovanthuan <ngovanthuan989@gmail.com>
2021-12-26 23:00:31 +0700

    thuandaycode

--- resources/views/dashboard/layout/master.blade.php
+++ resources/views/dashboard/layout/master.blade.php
@@ -4,7 +4,7 @@
 	<!--begin::Head-->
 	<head><base href="">
 		<meta charset="utf-8" />
-		<title>Admin-Quản lý hệ thống MacTree</title>
+		<title>Admin-Quản lý hệ thống BlackParis</title>
 		<meta name="description" content="Metronic admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link rel="canonical" href="https://keenthemes.com/metronic" />
@@ -20,7 +20,7 @@
 		<!--begin::Global Theme Styles(used by all pages)-->
 		<link href="{{asset('/admin/assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
 		<link href="{{asset('/admin/assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css" />
-		<link href="{{asset('/admin/assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+		<link href="{{asset('/admin/assets/css/style.bundle.min.css')}}" rel="stylesheet" type="text/css" />
 		<!--end::Global Theme Styles-->
 		<!--begin::Layout Themes(used by all pages)-->
 		<link href="{{asset('/admin/assets/css/themes/layout/header/base/light.css')}}" rel="stylesheet" type="text/css" />
